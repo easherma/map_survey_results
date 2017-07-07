40579582913e9d3480c075496f6c724a18d4359a
Eric Sherman <easherma@users.noreply.github.com>
2017-07-07 13:06:17 -0500

    Update getSubmissions.js

--- js/getSubmissions.js
+++ js/getSubmissions.js
@@ -36,30 +36,30 @@ $.ajax({
 
 var sql_statement='select the_geom, description, email, name, org, insert_time from bikeways';
 
-//old query for carto
+old query for carto
 
-//   $.getJSON('https://easherma.carto.com/api/v2/sql/?format=GeoJSON&q='+sql_statement, function(data) {
-// L.geoJson(data).addTo(map);
-//   });
-//   $.ajax({
-//     type: 'GET',
-//     url: 'https://'+config.cartoDBusername+'.carto.com/api/v2/sql/?format=GeoJSON',
-//     crossDomain: true,
-//     data: {"q":sql_statement},
-//     dataType: 'json',
-//     success: function(responseData, textStatus, jqXHR) {
-//       console.log("Data retrived");
-//       console.log(responseData);
-//       var submissionsStyle = L.geoJson(responseData, {
-//           onEachFeature: onEachFeature
-//       })
-//       layerOrder[layerOrder.length] = submissionsStyle;
-//       feature_group.addLayer(submissionsStyle).addTo(submissions);
-//
-//     },
-//     error: function (responseData, textStatus, errorThrown) {
-//         console.log(responseData);
-//
-//         console.log("Problem getting the data");
-//     }
-//   });
+  $.getJSON('https://easherma.carto.com/api/v2/sql/?format=GeoJSON&q='+sql_statement, function(data) {
+L.geoJson(data).addTo(map);
+  });
+  $.ajax({
+    type: 'GET',
+    url: 'https://'+config.cartoDBusername+'.carto.com/api/v2/sql/?format=GeoJSON',
+    crossDomain: true,
+    data: {"q":sql_statement},
+    dataType: 'json',
+    success: function(responseData, textStatus, jqXHR) {
+      console.log("Data retrived");
+      console.log(responseData);
+      var submissionsStyle = L.geoJson(responseData, {
+          onEachFeature: onEachFeature
+      })
+      layerOrder[layerOrder.length] = submissionsStyle;
+      feature_group.addLayer(submissionsStyle).addTo(submissions);
+
+    },
+    error: function (responseData, textStatus, errorThrown) {
+        console.log(responseData);
+
+        console.log("Problem getting the data");
+    }
+  });
