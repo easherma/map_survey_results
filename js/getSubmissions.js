0b75e974d1efa89579620ee6d7b8ffedf2a319b9
Eric Sherman <ericandrewsherman@gmail.com>
2017-07-27 12:42:54 -0500

    draw from ec2

--- js/getSubmissions.js
+++ js/getSubmissions.js
@@ -10,59 +10,66 @@
 //   })
 //
 
-// $.ajax({
-//   type: 'GET',
-//   url: 'https://api.osgisomg.com/api/data/',
-//   crossDomain: true,
-//   // data: {"q":sql_statement},
-//   dataType: 'json',
-//   contentType: 'application/json; charset=utf-8',
-//   success: function(responseData, textStatus, jqXHR) {
-//     console.log("Submissions retrived");
-//     console.log(JSON.stringify(responseData));
-//     var submissionsStyle = L.geoJson(responseData, {
-//         onEachFeature: onEachFeature
-//     })
-//     layerOrder[layerOrder.length] = submissionsStyle;
-//     feature_group.addLayer(submissionsStyle).addTo(submissions);
-//
-//   },
-//   error: function (responseData, textStatus, errorThrown) {
-//       console.log(JSON.stringify(responseData));
-//       console.log("Problem retriving submissions");
-//   }
-// });
+$.ajax({
+  type: 'GET',
+  url: 'https://api.osgisomg.com/api/data/',
+  crossDomain: true,
+  // data: {"q":sql_statement},
+  dataType: 'json',
+  contentType: 'application/json; charset=utf-8',
+  success: function(responseData, textStatus, jqXHR) {
+    console.log("Submissions retrived");
+    console.log(JSON.stringify(responseData));
+    var submissionsStyle = L.geoJson(responseData, {
+        style: function (feature) {
+  return {color: get_random_color()};
+},
+    // pointToLayer: function (feature, latlng) {
+    //         return L.circleMarker(latlng, geojsonMarkerOptions);
+    // }
+    // ,
+        onEachFeature: onEachFeatureSubmissions
+    });
+    layerOrder[layerOrder.length] = submissionsStyle;
+    feature_group.addLayer(submissionsStyle).addTo(submissions);
+
+  },
+  error: function (responseData, textStatus, errorThrown) {
+      console.log(JSON.stringify(responseData));
+      console.log("Problem retriving submissions");
+  }
+});
 
 
 var sql_statement="select the_geom, description, email, name, org, insert_time from bikeways where name != 'test'";
 
 // old query for carto
 
-  $.getJSON('https://easherma.carto.com/api/v2/sql/?format=GeoJSON&q='+sql_statement, function(data) {
-// L.geoJson(data).addTo(map);
-  });
-  $.ajax({
-    type: 'GET',
-    url: 'https://'+config.cartoDBusername+'.carto.com/api/v2/sql/?format=GeoJSON',
-    crossDomain: true,
-    data: {"q":sql_statement},
-    dataType: 'json',
-    success: function(responseData, textStatus, jqXHR) {
-      console.log("Data retrived");
-      console.log(responseData);
-      var submissionsStyle = L.geoJson(responseData, {
-          style: function (feature) {
-    return {color: get_random_color()};
-},
-          onEachFeature: onEachFeatureSubmissions
-      });
-      layerOrder[layerOrder.length] = submissionsStyle;
-      feature_group.addLayer(submissionsStyle).addTo(submissions);
-
-    },
-    error: function (responseData, textStatus, errorThrown) {
-        console.log(responseData);
-
-        console.log("Problem getting the data");
-    }
-  });
+//   $.getJSON('https://easherma.carto.com/api/v2/sql/?format=GeoJSON&q='+sql_statement, function(data) {
+// // L.geoJson(data).addTo(map);
+//   });
+//   $.ajax({
+//     type: 'GET',
+//     url: 'https://'+config.cartoDBusername+'.carto.com/api/v2/sql/?format=GeoJSON',
+//     crossDomain: true,
+//     data: {"q":sql_statement},
+//     dataType: 'json',
+//     success: function(responseData, textStatus, jqXHR) {
+//       console.log("Data retrived");
+//       console.log(responseData);
+//       var submissionsStyle = L.geoJson(responseData, {
+//           style: function (feature) {
+//     return {color: get_random_color()};
+// },
+//           onEachFeature: onEachFeatureSubmissions
+//       });
+//       layerOrder[layerOrder.length] = submissionsStyle;
+//       feature_group.addLayer(submissionsStyle).addTo(submissions);
+//
+//     },
+//     error: function (responseData, textStatus, errorThrown) {
+//         console.log(responseData);
+//
+//         console.log("Problem getting the data");
+//     }
+//   });
