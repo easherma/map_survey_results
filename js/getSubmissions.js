1618d1d472301ee18a95b880e2bbd5d7bcf5331c
Eric Sherman <ericandrewsherman@gmail.com>
2017-09-12 16:27:32 -0500

    showing results

--- js/getSubmissions.js
+++ js/getSubmissions.js
@@ -1,76 +1,67 @@
-// var sql = new cartodb.SQL({ user: 'easherma',format: 'geoJSON'});
-// sql.execute("SELECT * FROM bikeways")
-//   .done(function(data) {
-//       console.log(data);
-//     L.geoJson(data).addTo(map);
-//   })
-//   .error(function(errors) {
-//     // errors contains a list of errors
-//     console.log("errors:" + errors);
-//   })
-//
+var submissionsStyle = L.geoJson(submissions_scores, {
+    onEachFeature: onEachFeature,
+    style: style_comparesubmissions0_0
 
-$.ajax({
-  type: 'GET',
-  url: 'https://api.osgisomg.com/api/data/',
-  crossDomain: true,
-  // data: {"q":sql_statement},
-  dataType: 'json',
-  contentType: 'application/json; charset=utf-8',
-  success: function(responseData, textStatus, jqXHR) {
-    console.log("Submissions retrived");
-    console.log(JSON.stringify(responseData));
-    var submissionsStyle = L.geoJson(responseData, {
-        style: function (feature) {
-  return {color: get_random_color()};
-},
-    // pointToLayer: function (feature, latlng) {
-    //         return L.circleMarker(latlng, geojsonMarkerOptions);
-    // }
-    // ,
-        onEachFeature: onEachFeatureSubmissions
-    });
-    layerOrder[layerOrder.length] = submissionsStyle;
-    feature_group.addLayer(submissionsStyle).addTo(submissions);
 
-  },
-  error: function (responseData, textStatus, errorThrown) {
-      console.log(JSON.stringify(responseData));
-      console.log("Problem retriving submissions");
-      console.log(errorThrown);
-  }
 });
 
+layerOrder[layerOrder.length] = submissionsStyle;
+feature_group.addLayer(submissionsStyle).addTo(submissions);
 
-var sql_statement="select the_geom, description, email, name, org, insert_time from bikeways where name != 'test'";
-
-// old query for carto
-
-  $.getJSON('https://easherma.carto.com/api/v2/sql/?format=GeoJSON&q='+sql_statement, function(data) {
-// L.geoJson(data).addTo(map);
-  });
-  $.ajax({
-    type: 'GET',
-    url: 'https://'+config.cartoDBusername+'.carto.com/api/v2/sql/?format=GeoJSON',
-    crossDomain: true,
-    data: {"q":sql_statement},
-    dataType: 'json',
-    success: function(responseData, textStatus, jqXHR) {
-      console.log("Data retrived");
-      console.log(responseData);
-      var submissionsStyle = L.geoJson(responseData, {
-          style: function (feature) {
-    return {color: get_random_color()};
-},
-          onEachFeature: onEachFeatureSubmissions
-      });
-      layerOrder[layerOrder.length] = submissionsStyle;
-      feature_group.addLayer(submissionsStyle).addTo(submissions);
-
-    },
-    error: function (responseData, textStatus, errorThrown) {
-        console.log(responseData);
-
-        console.log("Problem getting the data");
+function style_comparesubmissions0_0(feature) {
+    if (feature.properties['run2_Score'] >= 1.673182 && feature.properties['run2_Score'] <= 3.236364 ) {
+        return {
+        opacity: 1,
+        color: 'rgba(254,240,217,1.0)',
+        dashArray: '',
+        lineCap: 'square',
+        lineJoin: 'bevel',
+        weight: 4.0,
+        fillOpacity: 0,
     }
-  });
+    }
+    if (feature.properties['run2_Score'] >= 3.236364 && feature.properties['run2_Score'] <= 4.687727 ) {
+        return {
+        opacity: 1,
+        color: 'rgba(253,204,138,1.0)',
+        dashArray: '',
+        lineCap: 'square',
+        lineJoin: 'bevel',
+        weight: 4.0,
+        fillOpacity: 0,
+    }
+    }
+    if (feature.properties['run2_Score'] >= 4.687727 && feature.properties['run2_Score'] <= 6.112273 ) {
+        return {
+        opacity: 1,
+        color: 'rgba(252,141,89,1.0)',
+        dashArray: '',
+        lineCap: 'square',
+        lineJoin: 'bevel',
+        weight: 4.0,
+        fillOpacity: 0,
+    }
+    }
+    if (feature.properties['run2_Score'] >= 6.112273 && feature.properties['run2_Score'] <= 7.419545 ) {
+        return {
+        opacity: 1,
+        color: 'rgba(227,74,51,1.0)',
+        dashArray: '',
+        lineCap: 'square',
+        lineJoin: 'bevel',
+        weight: 4.0,
+        fillOpacity: 0,
+    }
+    }
+    if (feature.properties['run2_Score'] >= 7.419545 && feature.properties['run2_Score'] <= 9.269545 ) {
+        return {
+        opacity: 1,
+        color: 'rgba(179,0,0,1.0)',
+        dashArray: '',
+        lineCap: 'square',
+        lineJoin: 'bevel',
+        weight: 4.0,
+        fillOpacity: 0,
+    }
+    }
+}
